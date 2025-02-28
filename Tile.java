fb65a619361f67161980b342f3ebe43ab493b0b9
ysaktan <ysaktan@proton.me>
2025-02-28 16:06:48 +0300

    feat: color mode

--- Tile.java
+++ Tile.java
@@ -1,5 +1,6 @@
 public class Tile {
-    
+
+    private static final boolean COLOR_MODE = false;
     int value;
     char color;
 
@@ -7,7 +8,8 @@ public class Tile {
      * Creates a tile using the given color and value, colors are represented
      * using the following letters: Y: Yellow, B: Blue, R: Red, K: Black
      * Values can be in the range [1,7]. There are four tiles of each color value
-     * combination (7 * 4 * 4) = 112 tiles, false jokers are not included in this game.
+     * combination (7 * 4 * 4) = 112 tiles, false jokers are not included in this
+     * game.
      */
     public Tile(int value, char color) {
         this.value = value;
@@ -18,36 +20,29 @@ public class Tile {
      * Compares tiles so that they can be added to the hands in order
      */
     public int compareTo(Tile t) {
-        if(getValue() < t.getValue()) {
+        if (getValue() < t.getValue()) {
             return -1;
-        }
-        else if(getValue() > t.getValue()) {
+        } else if (getValue() > t.getValue()) {
             return 1;
-        }
-        else{
-             if(colorNameToInt() < t.colorNameToInt()) {
+        } else {
+            if (colorNameToInt() < t.colorNameToInt()) {
                 return -1;
-            }
-            else if(colorNameToInt() > t.colorNameToInt()) {
+            } else if (colorNameToInt() > t.colorNameToInt()) {
                 return 1;
-            }
-            else{
+            } else {
                 return 0;
             }
         }
     }
 
     public int colorNameToInt() {
-        if(color == 'Y') {
+        if (color == 'Y') {
             return 0;
-        }
-        else if(color == 'B') {
+        } else if (color == 'B') {
             return 1;
-        }
-        else if(color == 'R') {
+        } else if (color == 'R') {
             return 2;
-        }
-        else {
+        } else {
             return 3;
         }
     }
@@ -56,7 +51,7 @@ public class Tile {
     public boolean canFormChainWith(Tile t) {
 
         // can make chain if same number but different color
-        if(t.getColor() != color && t.getValue() == value) {
+        if (t.getColor() != color && t.getValue() == value) {
             return true;
         } else {
             return false;
@@ -64,8 +59,70 @@ public class Tile {
 
     }
 
-    public String toString() {
+    enum Color {
+        BLACK(0, 0, 0),
+        YELLOW(255, 255, 0),
+        // YELLOW(255, 255, 143),
+        BLUE(0, 0, 255),
+        RED(255, 0, 0),
+        WHITE(255, 255, 255);
+
+        private Color(int r, int g, int b) {
+            this.r = r;
+            this.g = g;
+            this.b = b;
+        }
+
+        public String ansiFGString() {
+            return String.format("\33[38;2;%d;%d;%dm", r, g, b);
+        }
+
+        public String ansiBGString() {
+            return String.format("\33[48;2;%d;%d;%dm", r, g, b);
+        }
+
+        int r, g, b;
+    }
+
+    private String plainToString() {
         return "" + value + color;
+    }
+
+    private String colorToString() {
+        final String ANSI_WHITE_BG = "";
+        // final String ANSI_WHITE_BG = Color.WHITE.ansiBGString() + "\33[1m";
+        final String ANSI_RESET_ALL = "\33[0m";
+
+        String ansiStyle;
+        switch (color) {
+            case 'B':
+                ansiStyle = ANSI_WHITE_BG + Color.BLUE.ansiFGString();
+                break;
+            case 'Y':
+                ansiStyle = ANSI_WHITE_BG + Color.YELLOW.ansiFGString();
+                break;
+            case 'R':
+                ansiStyle = ANSI_WHITE_BG + Color.RED.ansiFGString();
+                break;
+            case 'K':
+                ansiStyle = ANSI_WHITE_BG + Color.BLACK.ansiFGString();
+                break;
+            default:
+                ansiStyle = "";
+                break;
+
+        }
+        return ansiStyle + value + ANSI_RESET_ALL;
+
+    }
+
+    @Override
+    public String toString() {
+        if (COLOR_MODE) {
+            return colorToString();
+        } else {
+            return plainToString();
+        }
     }
 
     public int getValue() {
@@ -75,5 +132,5 @@ public class Tile {
     public char getColor() {
         return color;
     }
-    
+
 }
